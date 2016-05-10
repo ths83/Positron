b46a2f66c5ef3b987a9ba9ec7f8047d6da168f74
martinez <martinez1nicolas@gmail.com>
2016-05-10 12:07:37 +0200

    unit tests services

--- javaRestServer/src/test/java/fr/univtln/groupc/CRestServicesTest.java
+++ javaRestServer/src/test/java/fr/univtln/groupc/CRestServicesTest.java
@@ -2,10 +2,14 @@ package fr.univtln.groupc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import fr.univtln.groupc.entities.CPlayerEntity;
+import fr.univtln.groupc.entities.CTeamEntity;
 import fr.univtln.groupc.server.CServer;
 import junit.framework.TestCase;
+
+import javax.ws.rs.core.Response;
 
 /**
  * Created by marti on 06/05/2016.
@@ -16,10 +20,24 @@ public class CRestServicesTest extends TestCase {
     WebResource mWebResource = c.resource(CServer.BASE_URI);
     ObjectMapper mMapper = new ObjectMapper();
 
+    public void testPostTeamService() throws Exception {
+        CTeamEntity lTeamToPost = new CTeamEntity.CTeamBuilder(150).color("vert").build();
+        String lJsonTeam = mMapper.writeValueAsString(lTeamToPost);
+        ClientResponse lResponse = mWebResource.path("/teams").type("application/json").accept("application/json").post(ClientResponse.class, lTeamToPost);
+        assertEquals(lResponse.getStatus(), 201);
+    }
+
+    public void testGetByIdTeamService() throws Exception {
+        CTeamEntity lTeamGotten = mMapper.readValue(mWebResource.path("/teams/150").accept("application/json").type("application/json").get(String.class), CTeamEntity.class);
+        assertEquals(lTeamGotten.getId(), 150);
+        assertEquals(lTeamGotten.getColor(), "vert");
+    }
+
     public void testPostPlayerService() throws Exception {
-        CPlayerEntity lPlayerToPost = new CPlayerEntity.CPlayerBuilder(1).email("email_de_test").energy(50).build();
+        CPlayerEntity lPlayerToPost = new CPlayerEntity.CPlayerBuilder(1).email("email_de_test").energy(50).latitude(125.3).longitude(142.9).build();
         String lJsonPlayer = mMapper.writeValueAsString(lPlayerToPost);
-        mWebResource.path("players/create").post(lPlayerToPost);
+        ClientResponse lResponse = mWebResource.path("players/create").type("application/json").accept("application/json").post(ClientResponse.class, lPlayerToPost);
+        assertEquals(lResponse.getStatus(), 201);
     }
 /*
     public void testGetByIdPlayerService() throws Exception {
