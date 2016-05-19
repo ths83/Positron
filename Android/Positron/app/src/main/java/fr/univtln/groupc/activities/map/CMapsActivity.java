47fa079868edd8f20b5847a3c96b697f0bb8b8b4
tp83 <ths.perron@gmail.com>
2016-05-19 20:04:24 +0200

    get keys, created link with key

--- Android/Positron/app/src/main/java/fr/univtln/groupc/activities/map/CMapsActivity.java
+++ Android/Positron/app/src/main/java/fr/univtln/groupc/activities/map/CMapsActivity.java
@@ -132,7 +132,7 @@ public class CMapsActivity extends FragmentActivity implements OnMapReadyCallbac
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
+            //                                          int [] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             return;
