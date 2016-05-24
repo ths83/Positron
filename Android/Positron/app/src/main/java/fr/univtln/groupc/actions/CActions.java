47b7a8b1dc25c8d009f6540dde78907ebbda4d7b
Xavier DURBEC <xavier-durbec@etud.univ-tln.fr>
2016-05-24 09:44:32 +0200

    refs:#2337 & refs:#2316

--- Android/Positron/app/src/main/java/fr/univtln/groupc/actions/CActions.java
+++ Android/Positron/app/src/main/java/fr/univtln/groupc/actions/CActions.java
@@ -2,11 +2,16 @@ package fr.univtln.groupc.actions;
 
 import android.util.Log;
 
+import java.util.Objects;
+
 import fr.univtln.groupc.entities.ABuildingEntity;
+import fr.univtln.groupc.entities.AObjectEntity;
 import fr.univtln.groupc.entities.CConsumableEntity;
 import fr.univtln.groupc.entities.CPlayerEntity;
 import fr.univtln.groupc.entities.CPortalEntity;
 import fr.univtln.groupc.entities.CResonatorEntity;
+import fr.univtln.groupc.entities.CShieldEntity;
+import fr.univtln.groupc.entities.CTurretEntity;
 
 /**
  * Created by xdurbec066 on 17/05/16.
@@ -40,7 +45,42 @@ public class CActions {
     }
 
 
-    public void piratage(CPortalEntity pPortal){
+    public void piratage(CPortalEntity pPortal, CPlayerEntity pPlayer){
+
+        if(pPortal.getTeam() != null){
+            if(pPortal.getTeam() == pPlayer.getTeam()){
+
+            }
+            else{
+
+            }
+
+        }
+        else{
+
+        }
         
+    }
+
+    public AObjectEntity createObject(int pTypeObjet , int pPortalLevel){
+      int lLevelObject = 0;
+      int lRarety = 0;
+
+       // TODO Calcule lLevelObject & lRarety
+
+     switch (pTypeObjet){
+
+         case(0):{
+                return (AObjectEntity) new CResonatorEntity.CResonatorBuilder(10).energyMax(lLevelObject*20).energy(lLevelObject*20).level(lLevelObject).build();
+         }
+         case(1): {
+             return (AObjectEntity) new CTurretEntity.CTurretBuilder(10).energy(lLevelObject * 50).energyMax(lRarety * 50).damage(10 * lRarety).build();
+         }
+         case(2):{
+             return (AObjectEntity) new CShieldEntity.CShieldBuilder(10).energy(lLevelObject * 50).energyMax(lRarety * 50).damage(10*lRarety).build();
+          }
+
+     }
+        return null;
     }
 }
